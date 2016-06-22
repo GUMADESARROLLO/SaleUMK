f67a7671a9c9c918f60e194b13883ed5c02a6082
Marangelo <ANALISTA.GUMA@GMAIL.COM>
2016-06-22 15:17:23 -0600

    Permsonalizacion de las vistas de la agenda el Formato de Recibo y anexo de variables Globales

--- app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/Lib/Variables.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrarywithfragments/Lib/Variables.java
@@ -5,6 +5,7 @@ package com.videumcorp.desarrolladorandroid.navigationdrawerandroiddesignsupport
  */
 public class Variables {
     private static String Inv_Articulo ;
+    private static String Inv_Cliente ;
 
     public Variables() {
     }
@@ -15,5 +16,13 @@ public class Variables {
 
     public static void setInv_Articulo(String inv_Articulo) {
         Inv_Articulo = inv_Articulo;
+    }
+
+    public static String getInv_Cliente() {
+        return Inv_Cliente;
+    }
+
+    public static void setInv_Cliente(String inv_Cliente) {
+        Inv_Cliente = inv_Cliente;
     }
 }
